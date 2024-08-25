91e5ddafd5f99f5b833cec29161375770df17a1c
cqb13 <cqb13.main@gmail.com>
2024-08-24 23:35:17 -0400

    updates

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ pub mod qr_code;
 use crate::qr_code::{EncodingMode, ErrorCorrectionLevel, QrCode};
 
 fn main() {
-    let data = "茗荷".to_string();
+    let data = "Hello, World!".to_string();
 
-    let qr_code = match QrCode::create(data, EncodingMode::Kanji, ErrorCorrectionLevel::High) {
+    let qr_code = match QrCode::create(data, EncodingMode::Byte, ErrorCorrectionLevel::High) {
         Ok(qr_code) => qr_code,
         Err(err) => {
             println!("Error: {}", err);
